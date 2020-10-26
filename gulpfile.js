5c34905b45ec5ac5dc3533d78834b1586524c1fe
UglanovEA <uglanovea@gmail.com>
2020-10-26 16:03:57 +0300

    ver 1.8 add webp plugin gulp

--- gulpfile.js
+++ gulpfile.js
@@ -93,6 +93,11 @@ function js() {
 
 function images() {
   return src(path.src.img)
+    .pipe(webp({
+      quality: 70
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
     .pipe(imagemin({
       interlaced: true,
       progressive: true,
