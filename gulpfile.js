8bab88396b1b6d2e17ded8e3227fca89d1eedc22
UglanovEA <uglanovea@gmail.com>
2020-10-27 14:09:40 +0300

    ver 2.3 add scss plagin gulp

--- gulpfile.js
+++ gulpfile.js
@@ -26,9 +26,11 @@ let path = {
 let { src, dest } = require('gulp'),
   gulp = require('gulp'),
   browsersync = require('browser-sync').create(),
-  pug = require('gulp-pug');
+  pug = require('gulp-pug'),
+  del = require('del'),
+  scss = require('gulp-sass');
 
-function browserSync(params) {
+function browserSync() {
   browsersync.init({
     server: {
       baseDir: './' + project_folder + '/'
@@ -45,9 +47,30 @@ function html() {
     .pipe(browsersync.stream())
 }
 
-let build = gulp.series(gulp.parallel(html));
-let watch = gulp.parallel(build, browserSync);
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: 'expanded'
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
 
+function watchFiles() {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(css, html));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
