9511ec4b873a24c31d6fd71af9102f2acf13aa02
UglanovEA <uglanovea@gmail.com>
2020-11-10 17:59:10 +0300

    ver 2.6 add nav-menu

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.querySelector('.nav-burger');
+const closeMenu = document.querySelector('.nav-menu__close');
+
+burgerMenu.addEventListener('click', function toggleClasses() {
+
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,28 @@
-function () {
-  const cursor = document.querySelector('.cursor');
-  const cursorRadius = Math.round(cursor.getBoundingClientRect().width / 2);
+// cursor
+const cursor = document.querySelector('.cursor');
+const cursorRadius = Math.round(cursor.getBoundingClientRect().width / 2);
 
-  const mouse = { x: 0, y: 0 };
-  const pos = { x: 0, y: 0 };
-  const ratio = 0.06;
+const mouse = { x: 0, y: 0 };
+const pos = { x: 0, y: 0 };
+const ratio = 0.06;
 
-  const draw = () => {
-    pos.x += (mouse.x - cursorRadius - pos.x) * ratio;
-    pos.y += (mouse.y - cursorRadius - pos.y) * ratio;
-    cursor.style.transform = `translate3d(${pos.x}px ,${pos.y}px, 0)`;
-  };
+const draw = () => {
+  pos.x += (mouse.x - cursorRadius - pos.x) * ratio;
+  pos.y += (mouse.y - cursorRadius - pos.y) * ratio;
+  cursor.style.transform = `translate3d(${pos.x}px ,${pos.y}px, 0)`;
+};
 
-  const update = e => {
-    mouse.x = e.clientX;
-    mouse.y = e.clientY;
-  };
+const update = e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+};
 
-  window.addEventListener('mousemove', update, { capture: false, passive: true });
+window.addEventListener('mousemove', update, { capture: false, passive: true });
 
-  function animate() {
-    draw();
-    requestAnimationFrame(animate);
-  }
-
+function animate() {
+  draw();
   requestAnimationFrame(animate);
 }
+
+requestAnimationFrame(animate);
+// cursor
